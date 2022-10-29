20b9e3f29a0302ac68b757de2447a6b31e339d32
Esa Varemo <esa@kuivanto.fi>
2022-10-29 03:26:34 +0300

    Annotate the BMP to line protocol converter

--- convert2.py
+++ convert2.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import sys
 
+# Read a 72x8 image
 image = Image.open(sys.argv[1])
 width, height = image.size
 assert height == 8
@@ -8,19 +9,22 @@ assert height == 8
 pixels = []
 
 for column in range(width-1, -1, -1):
+
+    # Build a single byte (display column) from 8 pixels:
     tmp = 0
     for row in range(height):
         pixel = image.getpixel((column, row))
         if pixel == 0:
             tmp |= 1 << row
 
-    #print(bin(tmp))
+    # escape the control-character by converting '.' -> '..'
     if tmp == ord('.'):
-        # escape
+        sys.stdout.buffer.write(b'..')
+    else:
+        # Write the raw byte
         sys.stdout.buffer.write(tmp.to_bytes(1, 'big'))
-    sys.stdout.buffer.write(tmp.to_bytes(1, 'big'))
-        
 
+# Strobe the shift register contents to display
 sys.stdout.buffer.write(b'.s')
 
 
